1d26d1d5155fd1d67394fa9fe80011ae41cfdbae
Saurav-Shrivastav <sshrivastav_be19@thapar.edu>
2020-09-28 12:49:08 +0530

    Querying users by ID

--- app/users/schema.py
+++ app/users/schema.py
@@ -10,6 +10,13 @@ class UserType(DjangoObjectType):
         # only_fields = ('id', 'email', 'username', 'password')         # in order to restrict the fields that are returned!
 
 
+class Query(graphene.ObjectType):
+    user = graphene.Field(UserType, id=graphene.Int(required=True))
+
+    def resolve_user(self, info, id):
+        return get_user_model().objects.get(id=id)
+
+
 class CreateUser(graphene.Mutation):
     user = graphene.Field(UserType)
 
